fcfd2c739de9268deedb041a9ff4273a34963011
hutt <blikksyt@gmail.com>
2025-09-05 23:51:22 +0300

    add: works, 50% advantages | fix: connections (css, imgs, scrips)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-// BURGER
+// Бургер меню
 const burger = document.querySelector('.burger')
 const headerContent = document.querySelector('.header__content')
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   headerContent.classList.toggle('active')
+})
+
+// Галерея (Fancybox)
+Fancybox.bind('[data-fancybox="gallery"]', {
+  placeFocusBack: false
+})
+
+// Слайдер (Swiper)
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    800: {
+      slidesPerView: 3,
+    },
+    1100: {
+      spaceBetween: 20,
+    }
+  }
 })
